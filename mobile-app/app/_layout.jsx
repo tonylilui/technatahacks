e0c0980657baff3bbae385c11ebba6083c57d455
Tony Li <tony.lilui416@gmail.com>
2024-09-28 12:24:57 -0400

    added expo-router and added nativewind (tailwindcss) styling

---
+++ mobile-app/app/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="text-3xl text-blue-500">App</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
